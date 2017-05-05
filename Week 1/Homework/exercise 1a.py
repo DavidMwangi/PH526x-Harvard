7e3efa5c8f4bed11ee2915f304823b0b4965af3b
DavidMwangi <david.mathenge98@gmail.com>
2017-05-05 14:52:07 +0300

    exercise 1b

--- Week 1/Homework/exercise 1a.py
+++ Week 1/Homework/exercise 1a.py
@@ -10,3 +10,34 @@ import string
 
 alphabet = string.ascii_letters
 
+#EXERCISE 1B
+
+"""
+
+The lower and upper cases of the English alphabet is stored as alphabet.
+Consider the sentence
+'Jim quickly realized that the beautiful gowns are expensive'.
+Create a dictionary count_letters with keys consisting of each unique
+letter in the sentence and values consisting of the number of times each
+letter is used in this sentence.
+Count both upper case and lower case letters separately in the dictionary.
+
+"""
+
+sentence = 'Jim quickly realized that the beautiful gowns are expensive'
+
+count_letters = {}
+#write your code here!
+count = 0
+
+for letter in sentence:
+    
+    if letter in alphabet:
+        
+        if letter in count_letters:
+            
+            count_letters[letter] += 1
+
+        else:
+            
+            count_letters[letter] = 1
